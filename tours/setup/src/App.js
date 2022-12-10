f1325d09586cb010da5e0e2357a6e90a6df21280
lkleinsorgen <lucca.kleinsorgen@gmail.com>
2022-12-10 13:31:06 -0300

    Added fetch API, useEffect & Conditional Return

--- tours/setup/src/App.js
+++ tours/setup/src/App.js
@@ -5,8 +5,42 @@ import Tours from "./Tours";
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tours-project";
 function App() {
-	const [loading, setLoading] = useState(true);
+	const [loading, setLoading] = useState(false);
 	const [tours, setTours] = useState([]);
+	const [error, setError] = useState(false);
+
+	const removeTour = (id) => {
+		const newTours = tours.filter((tour) => tour.id !== id);
+		setTours(newTours);
+	};
+
+	const fetchTours = async () => {
+		setLoading(true);
+		try {
+			const response = await fetch(`${url}`);
+			const tours = await response.json();
+			setLoading(false);
+			setError(false);
+			setTours(tours);
+		} catch (error) {
+			setLoading(false);
+			setError(true);
+		}
+	};
+
+	useEffect(() => {
+		fetchTours();
+	}, []);
+
+	if (error) {
+		return (
+			<main>
+				<h2 style={{ textAlign: "center " }}>Algo inesperado aconteceu!</h2>
+				<h2 style={{ textAlign: "center " }}>Tente Novamente!</h2>
+				<div style={{ width: "50%", marginTop: "2rem" }} className='underline'></div>
+			</main>
+		);
+	}
 
 	if (loading) {
 		return (
@@ -16,7 +50,29 @@ function App() {
 		);
 	}
 
-	return <Tours></Tours>;
+	if (tours.length === 0) {
+		return (
+			<main>
+				<div className='title'>
+					<h2>No Tours left.</h2>
+					<button
+						className='btn'
+						onClick={() => {
+							fetchTours();
+						}}>
+						Refresh tours
+					</button>
+				</div>
+			</main>
+		);
+	}
+
+	return (
+		<main>
+			<p className='btn'>Left: {tours.length}</p>
+			<Tours tours={tours} removeTour={removeTour} />
+		</main>
+	);
 }
 
 export default App;
