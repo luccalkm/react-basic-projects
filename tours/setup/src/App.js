10d73a42b337737c763750fce236de3b0359b928
lkleinsorgen <lucca.kleinsorgen@gmail.com>
2022-12-10 12:25:02 -0300

    (1/7) useEffect project

---
+++ tours/setup/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tours-project";
+function App() {
+	const [loading, setLoading] = useState(true);
+	const [tours, setTours] = useState([]);
+
+	if (loading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		);
+	}
+
+	return <Tours></Tours>;
+}
+
+export default App;
