9e959bbfaaa7b98f8be019e2fc9d0558cdaf30b1
lkleinsorgen <lucca.kleinsorgen@gmail.com>
2022-12-11 18:59:21 -0300

    Final Changes

--- reviews/setup/src/Review.js
+++ reviews/setup/src/Review.js
@@ -9,41 +9,64 @@ const Review = () => {
   const nextPerson = () => {
     setIndex((prevState) => {
       let newIndex = prevState + 1
-      if (newIndex >= people.length) {
-        return 0
-      } else {
-        return newIndex
-      }
-    })
-  }
-  const prevPerson = () => {
-    setIndex((prevState) => {
-      let newIndex = prevState - 1
-      if (newIndex <= 0) {
-        return people.length - 1
-      } else {
-        return newIndex
-      }
+      return checkIndex(newIndex)
     })
   }
 
+  const prevPerson = () => {
+    setIndex((prevState) => {
+      let newIndex = prevState - 1
+      return checkIndex(newIndex)
+    })
+  }
+
+  const checkIndex = (number) => {
+    console.log(number)
+    if (number >= people.length) {
+      return 0
+    } else if (number < 0) {
+      return people.length - 1
+    }
+    return number
+  }
+
+  const randomNumber = () => {
+    let random = Math.floor(Math.random() * people.length)
+    if (random === index) {
+      random = index + 1
+    }
+    setIndex(random)
+  }
+
   return (
-    <div style={{ display: 'flex', width: '100%', margin: '0 auto' }}>
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        margin: '0 auto',
+      }}
+    >
       <button className='prev-btn' onClick={prevPerson}>
         <FaChevronLeft />
       </button>
-      <article className='review'>
+      <article style={{ position: 'relative' }} className='review'>
         <div
           style={{
             background: 'hsl(205, 78%, 60%)',
-            width: '20px',
             color: 'white',
-            padding: '20px',
-            display: 'flex',
+            position: 'absolute',
+            height: '40px',
+            width: '40px',
             margin: '0',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            top: '1.5%',
+            left: '90%',
+            borderRadius: '200px',
           }}
         >
-          {index}
+          {index + 1}
         </div>
         <div className='img-container'>
           <img src={picture.large} alt={name} className='person-img' />
@@ -55,12 +78,13 @@ const Review = () => {
         <h3 className='author'>
           {name.first} {name.last}
         </h3>
-        <p className='job'>
+        <p style={{ fontWeight: '600', fontSize: '1rem' }} className='job'>
           {dob.date} - {dob.age}
         </p>
         <p className='info'>{text}</p>
-
-        <button className='random-btn'>Random</button>
+        <button className='random-btn' onClick={randomNumber}>
+          Random
+        </button>
       </article>
       <button className='next-btn' onClick={nextPerson}>
         <FaChevronRight />
