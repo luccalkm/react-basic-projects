05bba93a45bfdadb65ca6fe6cc73de0b883557cc
lkleinsorgen <lucca.kleinsorgen@gmail.com>
2022-12-11 23:39:47 -0300

    DEMO - Manual button approach

--- menu/setup/src/App.js
+++ menu/setup/src/App.js
@@ -1,10 +1,33 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from 'react'
+import Menu from './Menu'
+import Categories from './Categories'
+import items from './data'
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState([])
+
+  const filterItems = (category) => {
+    if (category.toLowerCase() === 'all') {
+      setMenuItems(items)
+      return
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  }
+
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
