070de4d414a8598daae9d4cea5942c54aabc9c70
lkleinsorgen <lucca.kleinsorgen@gmail.com>
2022-12-12 01:41:36 -0300

    Demo - Slider Autoplay

---
+++ slider/setup/src/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react'
+import { FaChevronRight, FaChevronLeft } from 'react-icons/fa'
+import { FaQuoteRight } from 'react-icons/fa'
+import data from './data'
+function App() {
+  const [people, setPeople] = useState(data)
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    if (index < 0) {
+      setIndex(people.length - 1)
+    }
+    if (index === people.length) {
+      setIndex(0)
+    }
+  }, [index])
+
+  useEffect(() => {
+    let slider = setTimeout(() => {
+      setIndex(index + 1)
+    }, 8000)
+    return () => clearInterval(slider)
+  }, [index])
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span>Reviews
+        </h2>
+      </div>
+      <div className='section-center'>
+        {people.map((person, personIndex) => {
+          const { id, quote, name, title, image } = person
+          let position = 'nextSlide'
+          if (personIndex === index) {
+            position = 'activeSlide'
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = 'lastSlide'
+          }
+
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className='person-img' />
+              <h4>{name}</h4>
+              <p className='title'>{title}</p>
+              <p className='text'>{quote}</p>
+              <FaQuoteRight className='icon' />
+            </article>
+          )
+        })}
+        <button className='prev' onClick={() => setIndex(index - 1)}>
+          <FaChevronLeft />
+        </button>
+        <button className='next' onClick={() => setIndex(index + 1)}>
+          <FaChevronRight />
+        </button>
+      </div>
+    </section>
+  )
+}
+
+export default App
