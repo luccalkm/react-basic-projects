079dadeb03c1b31074d7e769997ad2409f3a176e
lkleinsorgen <lucca.kleinsorgen@gmail.com>
2022-12-12 22:56:42 -0300

    Setup labels on each color and clipboard copy

--- color-generator/setup/src/SingleColor.js
+++ color-generator/setup/src/SingleColor.js
@@ -1,8 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ rgb, weight, index }) => {
+  const [alert, setAlert] = useState(false)
+  const styleRgb = rgb.join(',')
+
+  const hex = rgbToHex(...rgb)
+
+  return (
+    <article
+      onClick={(e) => {
+        setAlert(true)
+        navigator.clipboard.writeText(hex)
+      }}
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{
+        background: `rgb(${styleRgb})`,
+      }}
+    >
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hex}</p>
+      {alert && <p className='alert'>Copied to clipboard!</p>}
+    </article>
+  )
 }
 
 export default SingleColor
